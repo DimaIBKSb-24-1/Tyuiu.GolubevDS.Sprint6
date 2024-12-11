fa1bbea4d0ab24ed9a1b18718490f4c7e05ccc5c
голубев дима <maska.l.v@mail.ru>
2024-12-11 18:24:10 +0500

    6

--- Tyuiu.GolubevDS.Sprint6.Task6.V22.Lib/DataService.cs
+++ Tyuiu.GolubevDS.Sprint6.Task6.V22.Lib/DataService.cs
@@ -5,19 +5,43 @@ public class DataService : ISprint6Task6V22
 {
     public string CollectTextFromFile(string str, string path)
     {
-        List<string> firstWords = new List<string>();
+        string resStr = "";
         using (StreamReader reader = new StreamReader(path))
         {
+
             string line;
+
             while ((line = reader.ReadLine()) != null)
             {
-                var FirstWords = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).FirstOrDefault() ?? string.Empty;
-                if (FirstWords.Length > 0)
-                {
-                    firstWords.Add(FirstWords);
-                }
+                resStr += " " + line.Split(' ')[0];
+
+
             }
+
+
+            string input = resStr;
+
+            // Удаляем начальные и конечные пробельные символы
+            string result = input.Trim();
+
+            // Проверяем, есть ли пробелы в начале и в конце
+            if (result.StartsWith(" "))
+            {
+                // Если первый символ - пробел, удаляем его и следующий за ним символ
+                result = result.Substring(1);
+            }
+
+            if (result.EndsWith(" "))
+            {
+                // Если последний символ - пробел, удаляем его и предыдущий символ
+                result = result.Substring(0, result.Length - 1);
+            }
+
+
+
+            path = result;
+
         }
-        return string.Join(" ", firstWords);
+        return path;
     }
 }
