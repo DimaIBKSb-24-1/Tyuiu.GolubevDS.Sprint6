970438c1acbd6d893482567ca809bf38096362d2
голубев дима <maska.l.v@mail.ru>
2024-12-11 18:29:05 +0500

    6

--- Tyuiu.GolubevDS.Sprint6.Task6.V22.Lib/DataService.cs
+++ Tyuiu.GolubevDS.Sprint6.Task6.V22.Lib/DataService.cs
@@ -3,7 +3,7 @@ using tyuiu.cources.programming.interfaces.Sprint6;
 
 public class DataService : ISprint6Task6V22
 {
-    public string CollectTextFromFile(string str, string path)
+    public string CollectTextFromFile(string path)
     {
         string resStr = "";
         using (StreamReader reader = new StreamReader(path))
